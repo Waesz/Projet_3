6f725fc2ff5c5ea960482fc78816fbdd78b28070
Thierno Diallo <thierno@MacBook-Air-de-Thierno.local>
2023-11-16 11:00:31 +0100

    scenario user

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from typing import List
 from sqlalchemy.dialects.mysql import mysqlconnector
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
 
-DATABASE_URL = "postgresql://postgres:test@90.63.38.240/prj3"
+DATABASE_URL = "postgresql://postgres:Nenany20@localhost/projet3"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -26,7 +29,10 @@ class Task(Base):
         String(255), index=True
     )  # Spécifiez une longueur maximale de 255 caractères
     description = Column(String(255))
+    user_id = Column(Integer, ForeignKey('users.id'))  # ForeignKey to users table
 
+    # Establish the relationship between Task and User
+    user = relationship("User", back_populates="tasks")
 
 # Créez une classe modèle SQLAlchemy pour les utilisateurs
 class User(Base):
@@ -35,9 +41,11 @@ class User(Base):
     name = Column(String(255), unique=True)
     email = Column(String(255), unique=True)
     password = Column(String(255))
+    tasks = relationship("Task", back_populates="user")
 
 # Creéz une classe modèle Pydantic pour les tâches
 class UserCreate(BaseModel):
+    id: int
     name: str
     email: str
     password: str
@@ -50,8 +58,7 @@ class TaskCreate(BaseModel):
     status: str
     dateDeb: date
     dateFin: date
-    userId:int
-
+    user_id:int
 # Créez la table et toutes les autres tables
 Base.metadata.create_all(bind=engine)
 
@@ -65,6 +72,13 @@ async def register():
 async def login():
     return {"message": "Connexion d'un utilisateur"}
 
+# ROUTES POUR LES Users
+@app.get("/users", response_model=List[UserCreate])
+async def getUsers():
+    db = SessionLocal()
+    users = db.query(User).all()
+    db.close()
+    return [{"name": user.name, "email": user.email} for user in users]
 
 # ROUTES POUR LES TASKS
 @app.get("/tasks", response_model=List[TaskCreate])
@@ -122,3 +136,42 @@ async def delete_tasksbyid(task_id: int):
     db.commit()
     db.close()
     return task
+
+# un jeu de données pour les modeles
+
+# Fonction pour insérer des utilisateurs dans la base de données
+def create_users():
+    db = SessionLocal()
+    try:
+        users_data = [
+            {"name": "Alice", "email": "alice@example.com", "password": "pass123"},
+            {"name": "Bob", "email": "bob@example.com", "password": "pass456"},
+            {"name": "Charlie", "email": "charlie@example.com", "password": "pass789"},
+        ]
+        for user_data in users_data:
+            user = User(**user_data)
+            db.add(user)
+        db.commit()
+    finally:
+        db.close()
+
+# Fonction pour insérer des tâches dans la base de données
+def create_tasks():
+    db = SessionLocal()
+    try:
+        tasks_data = [
+            {"title": "Task 1", "description": "Description for Task 1", "user_id": 1},
+            {"title": "Task 2", "description": "Description for Task 2", "user_id": 2},
+            {"title": "Task 3", "description": "Description for Task 3", "user_id": 3},
+        ]
+        for task_data in tasks_data:
+            task = Task(**task_data)
+            db.add(task)
+        db.commit()
+    finally:
+        db.close()
+
+# Appel des fonctions pour insérer les données
+create_users()
+create_tasks()
+
